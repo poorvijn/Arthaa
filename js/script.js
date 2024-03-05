d3223e470bb72c902fe87a28ef66a3cd2c2a2e5b
Poorvi <pjnayak2003@gmail.com>
2024-03-05 09:15:47 +0530

    Initial API testing successful

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import {API_KEY, API_HOST} from './config.js';
+
 let clearBtn = document.getElementById('clearButton');
 let inputTextBox = document.getElementById('inputText')
 
@@ -5,3 +7,20 @@ clearBtn.addEventListener('click',()=>{
     // .value for form elements
     inputTextBox.value="";
 });
+ 
+const url = 'https://nlp-translation.p.rapidapi.com/v1/translate?text=Hello%2C%20world!!&to=es&from=en';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': API_KEY,
+		'X-RapidAPI-Host': API_HOST
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
